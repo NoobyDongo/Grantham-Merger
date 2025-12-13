dad8938941579181b975f3de0bc9972083d75653
NoobyDongo <itisonho@outlook.com>
2025-12-14 00:03:54 +0800

    Included the missing _build script
    Fixed an issue where the _build.js script is being accidentally blocked by .gitignore

---
+++ _build.js
@@ -0,0 +1,26 @@
+const now = new Date()
+const year = now.getFullYear()
+const month = String(now.getMonth() + 1).padStart(2, "0")
+const day = String(now.getDate()).padStart(2, "0")
+const hours = String(now.getHours()).padStart(2, "0")
+const minutes = String(now.getMinutes()).padStart(2, "0")
+const seconds = String(now.getSeconds()).padStart(2, "0")
+const dateString = `${year}${month}${day}_${hours}${minutes}${seconds}`
+const packageName = "temp"
+const outputFilename = `${packageName}-${dateString}.exe`
+
+import { execSync } from "child_process"
+
+try {
+  console.log("Building... \n")
+  execSync(
+    `pkg ./dist/bundle/bundle.js --target latest-win-x64 --output dist/${outputFilename}`,
+    {
+      stdio: "inherit",
+      encoding: "utf8",
+    }
+  )
+  console.log("Done. \n")
+} catch (e) {
+  console.error(e)
+}
