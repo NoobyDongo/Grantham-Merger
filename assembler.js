9a0ccfd30b8d0abcab89f5b23db3db526ed1ef27
NoobyDongo <itisonho@outlook.com>
2025-12-13 23:59:13 +0800

    Reformat assembler.js
    Haven't touched this file for over an year during which I had changed some of the formatter options
    
    Pushing this first, since big changes are coming

--- assembler.js
+++ assembler.js
@@ -1,115 +1,118 @@
-import ExcelJS from 'exceljs'
+import ExcelJS from "exceljs"
 
-const _singleBorder = { style: 'medium', color: { argb: 'FF000000' } }
+const _singleBorder = { style: "medium", color: { argb: "FF000000" } }
 
 const excelStyle = {
   align: {
-    vertical: 'middle',
-    horizontal: 'center'
+    vertical: "middle",
+    horizontal: "center",
   },
   border: {
-    full: ['top', 'left', 'bottom', 'right'].reduce((acc, key) => {
+    full: ["top", "left", "bottom", "right"].reduce((acc, key) => {
       acc[key] = _singleBorder
       return acc
-    }, {})
+    }, {}),
   },
   fontSize: {
-    table: 10
-  }
+    table: 10,
+  },
 }
 
-const bodyStyle = cell => {
+const bodyStyle = (cell) => {
   cell.border = excelStyle.border.full
   cell.font = {
-    size: excelStyle.fontSize.table
+    size: excelStyle.fontSize.table,
   }
   cell.alignment = {
     wrapText: true,
-    vertical: 'top'
+    vertical: "top",
   }
 }
 
-const bodyWarningStyle1 = cell => {
+const bodyWarningStyle1 = (cell) => {
   bodyStyle(cell)
   cell.fill = {
-    type: 'pattern',
-    pattern: 'solid',
-    fgColor: { argb: 'FFFFE066' }
+    type: "pattern",
+    pattern: "solid",
+    fgColor: { argb: "FFFFE066" },
   }
 }
-const bodyWarningStyle2 = cell => {
+const bodyWarningStyle2 = (cell) => {
   bodyStyle(cell)
   cell.fill = {
-    type: 'pattern',
-    pattern: 'solid',
-    fgColor: { argb: 'FFFFC107' }
+    type: "pattern",
+    pattern: "solid",
+    fgColor: { argb: "FFFFC107" },
   }
 }
 
-const headStyle = cell => {
+const headStyle = (cell) => {
   cell.fill = {
-    type: 'pattern',
-    pattern: 'solid',
-    fgColor: { argb: 'FFD9D9D9' }
+    type: "pattern",
+    pattern: "solid",
+    fgColor: { argb: "FFD9D9D9" },
   }
   cell.alignment = {
     ...cell.alignment,
-    wrapText: true
+    wrapText: true,
   }
   cell.font = {
-    size: excelStyle.fontSize.table
+    size: excelStyle.fontSize.table,
   }
   cell.border = excelStyle.border.full
 }
 
 const systemStyle = (cell, header) => {
   headStyle(cell)
-  cell.fill.fgColor = { argb: 'FFDCE6F1' }
+  cell.fill.fgColor = { argb: "FFDCE6F1" }
   if (!header)
     cell.alignment = {
       wrapText: true,
-      vertical: 'top'
+      vertical: "top",
     }
 }
 
 const warningStyle = (cell, header) => {
   systemStyle(cell, header)
-  cell.fill.fgColor = { argb: 'FFFFFFCC' }
+  cell.fill.fgColor = { argb: "FFFFFFCC" }
 }
 
 const errorStyle = (cell, header) => {
   systemStyle(cell, header)
-  cell.fill.fgColor = { argb: 'FFFCD5B4' }
+  cell.fill.fgColor = { argb: "FFFCD5B4" }
 }
 
 //#D8BFD8
 
 excelStyle.table = {
-  head: cell => headStyle(cell),
+  head: (cell) => headStyle(cell),
   error: (cell, header) => errorStyle(cell, header),
   warning: (cell, header) => warningStyle(cell, header),
   system: (cell, header) => systemStyle(cell, header),
-  body: cell => bodyStyle(cell),
-  warning1: cell => bodyWarningStyle1(cell),
-  warning2: cell => bodyWarningStyle2(cell)
+  body: (cell) => bodyStyle(cell),
+  warning1: (cell) => bodyWarningStyle1(cell),
+  warning2: (cell) => bodyWarningStyle2(cell),
 }
 
-function sanitizeWorksheetName (name) {
-  return name.replace(/[*?:\\/\[\]]/g, '_').substring(0, 31)
+function sanitizeWorksheetName(name) {
+  return name
+    .replace(/[\\/]/g, "-")
+    .replace(/[*?:\[\]]/g, "")
+    .substring(0, 31)
 }
 
-export function useExcelGenerator (schema, staticStyle) {
-  const columns = schema.map(column => ({ ...column }))
-  const stylesIn = columns.map(col => col._style || null)
+export function useExcelGenerator(schema, staticStyle) {
+  const columns = schema.map((column) => ({ ...column }))
+  const stylesIn = columns.map((col) => col._style || null)
   let totalRecords = 0
   // let recordNum = 0
 
-  function stylizeRow (row, style, height, header = false) {
+  function stylizeRow(row, style, height, header = false) {
     totalRecords++
 
     row.height = height
-    row.alignment = { vertical: 'middle', horizontal: 'center' }
-    let rowStyle = style || 'body'
+    row.alignment = { vertical: "middle", horizontal: "center" }
+    let rowStyle = style || "body"
 
     row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
       let colStyle = rowStyle
@@ -120,7 +123,7 @@ export function useExcelGenerator (schema, staticStyle) {
     })
   }
 
-  function parseRecord (worksheet, row, style) {
+  function parseRecord(worksheet, row, style) {
     // let corrected = false
     const rowValues = columns.reduce((acc, col, index) => {
       let value = col.get(row)
@@ -139,7 +142,7 @@ export function useExcelGenerator (schema, staticStyle) {
   }
 
   let warning = true
-  function parseRecords (worksheet, rows) {
+  function parseRecords(worksheet, rows) {
     for (const [key, records] of rows) {
       // console.log('key', key, records.length)
       if (records.length > 1) {
@@ -147,7 +150,7 @@ export function useExcelGenerator (schema, staticStyle) {
           parseRecord(
             worksheet,
             r,
-            key == 'no id' ? undefined : warning ? 'warning1' : 'warning2'
+            key == "no id" ? undefined : warning ? "warning1" : "warning2"
           )
         }
         warning = !warning
@@ -160,11 +163,11 @@ export function useExcelGenerator (schema, staticStyle) {
         worksheet,
         record,
         record.__overwritten
-          ? key == 'no id'
+          ? key == "no id"
             ? undefined
             : warning
-            ? 'warning1'
-            : 'warning2'
+            ? "warning1"
+            : "warning2"
           : undefined
       )
       if (record.__overwritten) {
@@ -173,11 +176,11 @@ export function useExcelGenerator (schema, staticStyle) {
     }
   }
 
-  return data => {
+  return (data) => {
     const workbook = new ExcelJS.Workbook()
     totalRecords = 0
 
-    Object.keys(data).map(key => {
+    Object.keys(data).map((key) => {
       const sanitizedKey = sanitizeWorksheetName(key)
       // console.log('sanitizedKey', sanitizedKey)
       const records = data[key]
@@ -186,15 +189,15 @@ export function useExcelGenerator (schema, staticStyle) {
       worksheet.columns = columns
 
       let header = worksheet.getRow(1)
-      stylizeRow(header, 'head', 50, true)
+      stylizeRow(header, "head", 50, true)
 
       //console.log('columns', columns, records)
-      worksheet.views = [{ state: 'frozen', ySplit: 1 }]
+      worksheet.views = [{ state: "frozen", ySplit: 1 }]
 
       const lastColumn = worksheet.getColumn(worksheet.columnCount).letter
       worksheet.autoFilter = {
-        from: 'A1',
-        to: `${lastColumn}1`
+        from: "A1",
+        to: `${lastColumn}1`,
       }
 
       parseRecords(worksheet, records)
